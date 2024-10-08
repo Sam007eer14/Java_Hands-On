124ccca184ec0ab8ab13ae73d94d785e134bbbb4
sameer <sameer007sharief.k@gmail.com>
2024-10-08 06:20:03 +0530

    Adding Project

---
+++ src/October/ex_02_10_2024/Multiplication_Of_Two.java
@@ -0,0 +1,10 @@
+package October.ex_02_10_2024;
+
+public class Multiplication_Of_Two {
+    public static void main(String[] args){
+        for(int i=0; i<=10; i++){
+            System.out.println("2 X " + i +" = " + (2*i));
+        }
+    }
+
+}
